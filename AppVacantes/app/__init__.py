7d2a69ca72b0dba7c8ddb3db36f190f9076a3784
Sergio Elias Robles Ignacio <roblessergioelias@gmail.com>
2025-10-16 11:49:37 -0600

    Se mando un mensaje desde run para la conexion a la base de datos

--- AppVacantes/app/__init__.py
+++ AppVacantes/app/__init__.py
@@ -1,10 +1,18 @@
 from flask import Flask
+from dotenv import load_dotenv
 from app.routes.usuarios_routes import usuarios_bp
 from .extensions import db
+
+load_dotenv()
+
+from config import Config
 
 #crear nuestra aplicacion, devolver una instancia de la clase Flask con las configuraciones asignadas
 def create_app():
     app = Flask(__name__)
+
+    #Cargar una confguración
+    app.config.from_object(Config)
 
     #Inicializar extensiones
     db.init_app(app)
--- AppVacantes/config.py
+++ AppVacantes/config.py
@@ -4,5 +4,9 @@ class Config:
     DEBUG = True
     PORT = 5000
     SQLALCHEMY_DATABASE_URI = (
-        f"mysql+pymsql://{os.getenv('DB_USER')}:"
+        f"mysql+pymysql://{os.getenv('DB_USER')}:"
+        f"{os.getenv('DB_PASSWORD')}@"
+        f"{os.getenv('DB_HOST')}/"
+        f"{os.getenv('DB_NAME')}"
     )
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
